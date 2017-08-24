a95696a85ba35fc4e0130c444fed943ba6bedb3f
Prav <pghimir@gmail.com>
2017-08-23 22:16:12 -0500

    able to populate persons in list using ngFor and person class instances

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Person} from "../../models/Person";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  persons: Person[] = [];
+
   constructor(public navCtrl: NavController) {
+    this.persons =  [
+      new Person('Praveen', 'praveen@gmail.com', '123 Main St.'),
+      new Person('Prashant', 'prashant@gmail.com', '456 Main St.'),
+      new Person('John', 'john@gmail.com', '789 Main St.'),
+    ];
   }
+
+
 }
